55b51fa937280181a2a91dc0618a25de360da8ac
RunSoo <109501116+RunSoo@users.noreply.github.com>
2025-06-03 22:33:29 +0900

    [level 3] Title: 네트워크, Time: 1.17 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,53 +1,53 @@
 import java.util.*;
 class Solution {
-    public static class UnionFind {
-        int[] parent, rank;
-        
+    static class UnionFind {
+        int[] rank;
+        int[] parent;
         UnionFind(int n) {
-            this.parent = new int[n];
             this.rank = new int[n];
-            for (int i=0; i<n; i++) {
-                this.parent[i] = i;
+            this.parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
             }
         }
         
-        int find(int idx) {
-            if (parent[idx]!=idx) {
-                return find(parent[idx]);
+        public int find(int x) {
+            if (parent[x] != x) {
+                return find(parent[x]);
             }
-            return parent[idx];
+            return parent[x];
         }
         
-        void union(int idx1, int idx2) {
+        public void union(int idx1, int idx2) {
             int root1 = find(idx1);
             int root2 = find(idx2);
-            
-            if (root1!=root2) {
-                if (rank[root1]>rank[root2]){
-                    parent[root2] = root1;
-                } else if (rank[root1]<rank[root2]) {
+            if (root1 != root2) {
+                if (rank[root1] < rank[root2]) {
                     parent[root1] = root2;
-                } else {
+                } else if (rank[root1] > rank[root2]) {
                     parent[root2] = root1;
+                } else {
                     rank[root1]++;
+                    parent[root2] = root1;
                 }
             }
         }
-    }
-    
+    } 
     public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
         UnionFind uf = new UnionFind(n);
-        for (int r=0; r<n; r++) {
-            for (int c=0; c<n; c++) {
-                if (r!=c && computers[r][c]==1) {
+        for (int r = 0; r < computers.length; r++) {
+            for (int c = 0; c < computers[0].length; c++) {
+                if (computers[r][c] == 1) {
                     uf.union(r, c);
                 }
             }
         }
         
         Set<Integer> set = new HashSet<>();
-        for (int i=0; i<n; i++) {
-            set.add(uf.find(i));
+        for (int i = 0; i < uf.parent.length; i++) {
+            set.add(uf.find(uf.parent[i]));
         }
         
         return set.size();
