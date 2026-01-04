3d6cdde8ced5e7a97a9cd161a38e5217b5a2df11
RunSoo <109501116+RunSoo@users.noreply.github.com>
2026-01-04 17:23:33 +0900

    [level 1] Title: 잔챙이 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
+++ 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
@@ -1,4 +1,4 @@
--- 코드를 작성해주세요
-select count(*) as fish_count
-from fish_info
-where length <= 10  or length is null
+    -- 코드를 작성해주세요
+    select count(*) as FISH_COUNT
+    from fish_info
+    where length <= 10  or length is null
