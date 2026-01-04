f87a1c3a31095ac876192e9026a1c0c2388afa4b
RunSoo <109501116+RunSoo@users.noreply.github.com>
2026-01-04 17:16:53 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,5 +1,5 @@
-SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPERS
-WHERE SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') > 0
-    OR SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') > 0
-ORDER BY ID;
+SELECT DISTINCT d.id, d.email, d.first_name, d.last_name
+FROM skillcodes s, developers d
+WHERE (s.name = 'C#' OR s.name = 'Python')
+    AND (d.skill_code & s.code) > 0
+ORDER BY d.id;
